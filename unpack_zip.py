10fb68f6a013b9b10b858efc4dea276b958b78f6
robinrobinzon <z39rbk2oehpg@mail.ru>
2018-05-18 16:45:53 +0300

    fix unpack_zip.py

--- unpack_zip.py
+++ unpack_zip.py
@@ -1,18 +1,24 @@
 import os
 import subprocess
 
-root_folder = '/path/to/root/folder'
+_root_folder = '/path/to/root/folder'
 
 
-""" This script will extract each archive in separate folder. """
+""" This script will extract each zip archive in separate folder. """
 
 
-folders_to_process = []
-for root, dirs, files in os.walk(root_folder):
-    for folder in dirs:
-        folders_to_process.append(os.path.join(root, folder))
+def extract_zip(root_folder):
+    for root, dirs, files in os.walk(root_folder):
+        for file in files:
+            file_name, file_extension = file.rsplit('.', 1)
+            if 'zip' != file_extension:
+                continue
+            file_path = os.path.join(root, file)
+            print(file_path)
+            subprocess.call('''unzip -u -d "{}" "{}"'''.format(file_name, file_path), cwd=root, shell=True,
+                            stdout=subprocess.DEVNULL)
 
-for folder in sorted(folders_to_process):
-    print(folder)
-    subprocess.call('''for f in *.zip; do unzip -d "${f%*.zip}" "$f"; done''', cwd=folder, shell=True)
+
+if __name__ == '__main__':
+    extract_zip(_root_folder)
 
